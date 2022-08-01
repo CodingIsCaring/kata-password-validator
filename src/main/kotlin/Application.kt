c740fa41af6b5a9ab51ca1275e149a7e11aa0e96
Maria Soria <maria.it3@gmail.com>
2022-08-01 20:13:51 +0100

    First iteration. Underscore condition pending

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,7 +1,43 @@
 class Application {
     companion object {
-        fun helloWorld(): String {
-            return "Hello world!"
+        fun validate(password: String): Boolean {
+            if (!isLongEnough(password)) {
+                return false
+            }
+
+            if (hasNoDigits(password)) {
+                return false
+            }
+
+            if (hasNoUppercase(password)) {
+                return false
+            }
+
+            if (hasNoLowercase(password)) {
+                return false
+            }
+
+            return true
         }
+
+        private fun hasNoLowercase(password: String): Boolean {
+            val totalLowercaseLetters = password.count { it.isLowerCase() }
+            val hasNoLowercase = totalLowercaseLetters == 0
+            return hasNoLowercase
+        }
+
+        private fun hasNoUppercase(password: String): Boolean {
+            val totalUppercaseLetters = password.count { it.isUpperCase() }
+            val hasNoUppercase = totalUppercaseLetters == 0
+            return hasNoUppercase
+        }
+
+        private fun hasNoDigits(password: String): Boolean {
+            val totalDigits = password.count { it.isDigit() }
+            val hasNoDigits = totalDigits == 0
+            return hasNoDigits
+        }
+
+        private fun isLongEnough(password: String) = password.length >= 9
     }
 }
--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,8 +1,36 @@
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class ApplicationTest {
     @Test
-    fun say_hello_world() {
-        kotlin.test.assertEquals("Hello world!", Application.helloWorld())
+    fun `should be invalid password when has less than 8 characters`() {
+        assertFalse(Application.validate("pass"))
     }
+
+    @Test
+    fun `invalid password when has 8 characters`() {
+        assertFalse(Application.validate("password"))
+    }
+
+    @Test
+    fun `should be valid password`() {
+        assertTrue(Application.validate("p4sswOrds"))
+    }
+
+    @Test
+    fun `should be invalid if does not contain at least one capital letter`() {
+        assertFalse(Application.validate("passwords"))
+    }
+
+    @Test
+    fun `should be invalid if does not contain at least one lower case`() {
+        assertFalse(Application.validate("PASSWORDS"))
+    }
+
+    @Test
+    fun `should be invalid if does not contain at least a number`() {
+        assertFalse(Application.validate("passwords"))
+    }
+
 }
